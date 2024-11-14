d1d2e58f947c5b24f8ba9c6adbe909770d4de870
Gabriela Schlemper <gabschlemper@gmail.com>
2024-11-14 19:51:45 -0300

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: [
+    './components/**/*.{vue,js}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+    './app.vue'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
